b5edbb780e64aef8ef77350e7d2d2f64f9306995
Denilton Darold <denilton.d@gmail.com>
2016-07-21 19:32:02 -0300

    alteracoes 21/07 4

--- static/dist/assets/js/slider_script.js
+++ static/dist/assets/js/slider_script.js
@@ -2,23 +2,23 @@ $(document).ready(function() {
 /*    $('#myCarousel').carousel({
 	    interval: 10000
 	})*/
-
+	
     $( "#slide1" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37900/live/live/ponte-hercilio-luz?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Ponte Hercílio Luz"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37900/live/live/ponte-hercilio-luz?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Ponte Hercílio Luz"></iframe>');
         $('#currentSlide_title').html('Ponte Hercílio Luz');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_ponte.jpg'>");
 
     });
 
     $( "#slide2" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/39444/live/live/kiosque-beiramar-norte?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Kioske"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/39444/live/live/kiosque-beiramar-norte?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Kioske"></iframe>');
         $('#currentSlide_title').html('Kioske');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_anuncie.png'>");
 
     });
 
     $( "#slide3" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37866/live/live/beira-mar-norte?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Beira Mar Norte"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37866/live/live/beira-mar-norte?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Beira Mar Norte"></iframe>');
         $('#currentSlide_title').html('Beira Mar Norte');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_gelatoo.png'>");
 
@@ -26,18 +26,18 @@ $(document).ready(function() {
 
 //    Slide 2
     $( "#slide4" ).click(function() {
-        $('#currentSlide').html('    <iframe src="http://monitore.maaxcam.com.br/embed/37903/live/live/camera-catedral?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Catedral"></iframe>');
+        $('#currentSlide').html('    <iframe src="http://monitore.maaxcam.com.br/embed/37903/live/live/camera-catedral?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Catedral"></iframe>');
         $('#currentSlide_title').html('Catedral');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_catedral.jpg'>");
     });
     $( "#slide5" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37864/live/live/parque-de-coqueiros?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Coqueiros"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37864/live/live/parque-de-coqueiros?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Coqueiros"></iframe>');
         $('#currentSlide_title').html('Coqueiros');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_coqueiros.jpg'>");
 
     });
     $( "#slide6" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/38138/live/live/beiramar-koxixos?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Koxixos"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/38138/live/live/beiramar-koxixos?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Koxixos"></iframe>');
         $('#currentSlide_title').html('Koxixos');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_koxixos.gif'>");
 
@@ -45,19 +45,19 @@ $(document).ready(function() {
 
 //  Slide 3
     $( "#slide7" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37910/live/live/mirante-lagoa?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Mirante da Lagoa"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37910/live/live/mirante-lagoa?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Mirante da Lagoa"></iframe>');
         $('#currentSlide_title').html('Mirante da Lagoa');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_anuncie.png'>");
 
     });
     $( "#slide8" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37594/live/live/ponte-lagoa-da-conceicao?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Ponte da Lagoa"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37594/live/live/ponte-lagoa-da-conceicao?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Ponte da Lagoa"></iframe>');
         $('#currentSlide_title').html('Ponte da Lagoa');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_anuncie.png'>");
 
     });
     $( "#slide9" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/38141/live/live/marina-sto-antonio-de-lisboa?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Marina Santo Antônio de Lisboa"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/38141/live/live/marina-sto-antonio-de-lisboa?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Marina Santo Antônio de Lisboa"></iframe>');
         $('#currentSlide_title').html('Marina Sto Antônio de Lisboa');
         $('#propaganda_slide').html("<img src='/site_media/static/assets/images/publicidade_sto_antonio.jpg'>");
 
@@ -65,11 +65,11 @@ $(document).ready(function() {
     
 //  Slide 4
     $( "#slide10" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37902/live/live/camera-kitesurf?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Câmera Kit Surf"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37902/live/live/camera-kitesurf?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Câmera Kit Surf"></iframe>');
         $('#currentSlide_title').html('Câmera Kite-Surf');
     });
     $( "#slide11" ).click(function() {
-        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37906/live/live/joaquina?autoplay=false&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Praia da Joaquina"></iframe>');
+        $('#currentSlide').html('<iframe src="http://monitore.maaxcam.com.br/embed/37906/live/live/joaquina?autoplay=true&sound=true" width="685px" height="400px" frameborder="0" allowfullscreen title="Praia da Joaquina"></iframe>');
         $('#currentSlide_title').html('Praia da Joaquina');
     });
     
