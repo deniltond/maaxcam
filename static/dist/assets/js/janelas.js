a641d02732536c32dc2ab97b575f1038f87d222a
Denilton Darold <denilton.d@gmail.com>
2016-07-22 18:19:19 -0300

    alteracoes 22/07 4

--- static/dist/assets/js/janelas.js
+++ static/dist/assets/js/janelas.js
@@ -58,7 +58,7 @@ $('.cameras').slick({
   dots: false,
   infinite: true,
   speed: 300,
-//  slidesToShow: 4,
+  slidesToShow: 5,
   slidesToScroll: 1,
   adaptiveHeight: true,
   onAfterChange : makeFullLink,
